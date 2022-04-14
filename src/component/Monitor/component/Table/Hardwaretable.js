82f2cc17a31156c0585f51f117b901068df94e1e
Akram <97782617+AYoussoufi@users.noreply.github.com>
2022-04-14 17:09:13 +0000

    UPDATING MONITOR PAGE

--- src/component/Monitor/component/Table/Hardwaretable.js
+++ src/component/Monitor/component/Table/Hardwaretable.js
@@ -1,52 +1,92 @@
 import "./Hardwaretable.css";
+import editimg from "./edit-icon.png";
+import correctimg from "./509013.webp";
+import React, { useState } from "react";
 
 function Hardwaretable() {
   //THOSE ARE TABLE VARIABLES.
-  const Sensor = "";
   const AlarmProfile = "";
   const Value = "";
   const State = "";
 
-  //THIS FUNCTION FOR DELETE BUTTON
+  const [Sensor, setSensor] = useState("");
 
-  // const checkedbox = document.querySelector(".checkbox:checked").parentElement.parentElement;
-  // const checkbox = document.querySelectorAll(".checkbox");
-  // const delbutt = document.querySelector(".button--group");
-  //MAKE THE DEL BUTTON WORK
-  const showdelbutt = function () {
-    const checkedbox = document.querySelectorAll(".checkbox:checked");
+  const editbutton = function (event) {
+    setSensor("");
+    const editimage = document.querySelector(".Modify .edit");
+    const correctimage = document.querySelector(".Modify .correct");
+    const sensorInput = document.querySelector(".editsensor");
+    const newSensor = document.querySelector(".editsensor").value;
+    sensorInput.style.display = "inline";
+    correctimage.style.display = "inline";
+    editimage.style.display = "none";
+    if (event.key === "Enter") {
+      setSensor(newSensor);
+      sensorInput.style.display = "none";
+      correctimage.style.display = "none";
+      editimage.style.display = "block";
+    }
+  };
+
+  const showdelbutt =
+    function () /* THIS FUNCTION SHOW AND ACTIVATE DELETE BUTTON */ {
+      const checkedbox = document.querySelectorAll(".checkbox:checked");
+      const delbutt = document.querySelector(".button--group");
+      const checkbox = document.querySelectorAll(".checkbox");
+      for (let i = 0; i <= checkbox.length; i++) {
+        if (checkedbox.length >= 1) {
+          delbutt.style.opacity = "1";
+          delbutt.style.cursor = "pointer";
+        } else if (checkedbox.length === 0) {
+          delbutt.style.opacity = "0.1";
+          delbutt.style.cursor = "not-allowed";
+        }
+      }
+    };
+
+  const confirmdelete = function () /* SHOW CONFIRM BOX FOR DELETE ITEMS */ {
+    const messagebox = document.querySelector(".confirm--message");
     const delbutt = document.querySelector(".button--group");
-    const checkbox = document.querySelectorAll(".checkbox");
 
-    for (let i = 0; i <= checkbox.length; i++) {
-      if (checkedbox.length >= 1) {
-        delbutt.style.opacity = "1";
-        delbutt.style.cursor = "pointer";
-      } else if (checkedbox.length === 0) {
-        delbutt.style.opacity = "0.1";
-        delbutt.style.cursor = "not-allowed";
+    if (delbutt.style.cursor === "pointer") {
+      if (messagebox.style.display === "flex") {
+        messagebox.style.display = "none";
+      } else {
+        messagebox.style.display = "flex";
       }
     }
   };
 
-  const showconfirmdel = function () {
-    const checkedbox = document.querySelectorAll(".checkbox:checked");
-    const delbutt = document.querySelector(".button--group");
-    for (let i = 0; i <= checkedbox.length; i++) {
-      checkedbox[i].parentElement.parentElement.remove();
-      delbutt.style.opacity = "0.1";
-      delbutt.style.cursor = "not-allowed";
-    }
-  };
+  const deleteselecteditems =
+    function () /* DELETE SELECTED ITEMS THAT EXISTED IN CHECKED ARRAY CHECKBOX */ {
+      const checkedbox = document.querySelectorAll(".checkbox:checked");
+      const delbutt = document.querySelector(".button--group");
+      const messagebox = document.querySelector(".confirm--message");
+      messagebox.style.display = "none";
+      for (let i = 0; i <= checkedbox.length; i++) {
+        checkedbox[i].parentElement.parentElement.remove();
+        delbutt.style.opacity = "0.1";
+        delbutt.style.cursor = "not-allowed";
+      }
+    };
 
   return (
     <div className="table--container">
       <div className="confirm--message">
-        <div className="message--overlay"></div>
+        <div className="message--overlay" onClick={confirmdelete}></div>
         <div className="message--box">
-          <div>s</div>
-          <div>s</div>
-          <div>s</div>
+          <button className="close--message" onClick={confirmdelete}>
+            X
+          </button>
+          <div className="message--title">ARE YOU SURE ?</div>
+          <div className="message--button--container">
+            <button className="yes--button" onClick={deleteselecteditems}>
+              YES
+            </button>
+            <button className="non--button" onClick={confirmdelete}>
+              CLOSE
+            </button>
+          </div>
         </div>
       </div>
       <div className="table">
@@ -80,88 +120,38 @@ function Hardwaretable() {
                   onClick={showdelbutt}
                   className="checkbox"
                   type="checkbox"
-                  id="hardware"
-                  name="hardware"
-                  value="hardware"
                 />
               </td>
-              <td>1</td>
+              <td>
+                {Sensor}
+                <input className="editsensor" onKeyPress={editbutton}></input>
+              </td>
               <td>{AlarmProfile}</td>
               <td>{Value}</td>
               <td>{State}</td>
               <td>
                 <div className="Modify">
-                  <div></div>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <input
-                  onClick={showdelbutt}
-                  className="checkbox"
-                  type="checkbox"
-                  id="hardware"
-                  name="hardware"
-                  value="hardware"
-                />
-              </td>
-              <td>2</td>
-              <td>{AlarmProfile}</td>
-              <td>{Value}</td>
-              <td>{State}</td>
-              <td>
-                <div className="Modify">
-                  <div></div>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <input
-                  onClick={showdelbutt}
-                  className="checkbox"
-                  type="checkbox"
-                  id="hardware"
-                  name="hardware"
-                  value="hardware"
-                />
-              </td>
-              <td>3</td>
-              <td>{AlarmProfile}</td>
-              <td>{Value}</td>
-              <td>{State}</td>
-              <td>
-                <div className="Modify">
-                  <div></div>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <input
-                  onClick={showdelbutt}
-                  className="checkbox"
-                  type="checkbox"
-                  id="hardware"
-                  name="hardware"
-                  value="hardware"
-                />
-              </td>
-              <td>4</td>
-              <td>{AlarmProfile}</td>
-              <td>{Value}</td>
-              <td>{State}</td>
-              <td>
-                <div className="Modify">
-                  <div></div>
+                  <div>
+                    <img
+                      className="edit"
+                      src={editimg}
+                      alt="Edit your hardware"
+                      onClick={editbutton}
+                    ></img>
+                    <img
+                      className="correct"
+                      src={correctimg}
+                      alt="Edit your hardware"
+                      onClick={editbutton}
+                    ></img>
+                  </div>
                 </div>
               </td>
             </tr>
           </tbody>
           <tfoot />
         </table>
-        <button className="button--group" onClick={showconfirmdel}>
+        <button className="button--group" onClick={confirmdelete}>
           <div className="button--group--arrow"></div> Delete
         </button>
       </div>
