00ab2daabfb20ed7180779d46df31b22060ae1e3
Akram <97782617+AYoussoufi@users.noreply.github.com>
2022-05-17 13:42:50 +0100

    UPDATE

--- src/component/Monitor/component/Action/HardwareAction.js
+++ src/component/Monitor/component/Action/HardwareAction.js
@@ -1,10 +1,64 @@
 import "./HardwareAction.css";
 
 function HardwareAction() {
+  // const overlayAdd = document.querySelector(".overlay--add--container");
+  // const submitAdd = document.querySelector("form div .form--add--submit");
+  // const closeAdd = document.querySelector(".window--add--box button");
+
+  const clshowbox = function () {
+    const windowAllbox = document.querySelector(".window--add--container");
+    if (
+      windowAllbox.style.display === "none" ||
+      windowAllbox.style.display === ""
+    ) {
+      windowAllbox.style.display = "flex";
+    } else if (windowAllbox.style.display === "flex") {
+      windowAllbox.style.display = "none";
+    }
+  };
+
   return (
     <div className="action--container">
+      <div className="overlay--add--container"></div>
+      <div className="window--add--container">
+        <div className="overlay--add--container" onClick={clshowbox}></div>
+        <div className="window--add--box">
+          <button onClick={clshowbox}>x</button>
+          <div>
+            <h2>ADD YOUR HARDWARE</h2>
+          </div>
+          <div className="form--add--box">
+            <form /*onSubmit={submitHandler}*/>
+              <div>
+                <span>Title</span>
+                <input />
+              </div>
+              <div>
+                <span>Title</span>
+                <input />
+              </div>
+              <div>
+                <span>Title</span>
+                <input />
+              </div>
+              <div>
+                <span>Title</span>
+                <input />
+              </div>
+              <div>
+                <input
+                  type="submit"
+                  className="form--add--submit"
+                  value="VALID"
+                  onClick={clshowbox}
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
       <div className="add--button--container">
-        <button className="add--button">
+        <button className="add--button" onClick={clshowbox}>
           +
           <div className="arrow">
             <div className="message">Add your hardware</div>
--- src/component/Monitor/component/Table/Hardwaretable.js
+++ src/component/Monitor/component/Table/Hardwaretable.js
@@ -1,48 +1,9 @@
 import "./Hardwaretable.css";
-import editimg from "./edit-icon.png";
-import correctimg from "./509013.webp";
-import React, { useState } from "react";
+
+import TableRow from "./component/TableRow/TableRow";
 
 function Hardwaretable() {
   //THOSE ARE TABLE VARIABLES.
-  const AlarmProfile = "";
-  const Value = "";
-  const State = "";
-
-  const [Sensor, setSensor] = useState("");
-
-  const editbutton = function (event) {
-    setSensor("");
-    const editimage = document.querySelector(".Modify .edit");
-    const correctimage = document.querySelector(".Modify .correct");
-    const sensorInput = document.querySelector(".editsensor");
-    const newSensor = document.querySelector(".editsensor").value;
-    sensorInput.style.display = "inline";
-    correctimage.style.display = "inline";
-    editimage.style.display = "none";
-    if (event.key === "Enter") {
-      setSensor(newSensor);
-      sensorInput.style.display = "none";
-      correctimage.style.display = "none";
-      editimage.style.display = "block";
-    }
-  };
-
-  const showdelbutt =
-    function () /* THIS FUNCTION SHOW AND ACTIVATE DELETE BUTTON */ {
-      const checkedbox = document.querySelectorAll(".checkbox:checked");
-      const delbutt = document.querySelector(".button--group");
-      const checkbox = document.querySelectorAll(".checkbox");
-      for (let i = 0; i <= checkbox.length; i++) {
-        if (checkedbox.length >= 1) {
-          delbutt.style.opacity = "1";
-          delbutt.style.cursor = "pointer";
-        } else if (checkedbox.length === 0) {
-          delbutt.style.opacity = "0.1";
-          delbutt.style.cursor = "not-allowed";
-        }
-      }
-    };
 
   const confirmdelete = function () /* SHOW CONFIRM BOX FOR DELETE ITEMS */ {
     const messagebox = document.querySelector(".confirm--message");
@@ -114,40 +75,8 @@ function Hardwaretable() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>
-                <input
-                  onClick={showdelbutt}
-                  className="checkbox"
-                  type="checkbox"
-                />
-              </td>
-              <td>
-                {Sensor}
-                <input className="editsensor" onKeyPress={editbutton}></input>
-              </td>
-              <td>{AlarmProfile}</td>
-              <td>{Value}</td>
-              <td>{State}</td>
-              <td>
-                <div className="Modify">
-                  <div>
-                    <img
-                      className="edit"
-                      src={editimg}
-                      alt="Edit your hardware"
-                      onClick={editbutton}
-                    ></img>
-                    <img
-                      className="correct"
-                      src={correctimg}
-                      alt="Edit your hardware"
-                      onClick={editbutton}
-                    ></img>
-                  </div>
-                </div>
-              </td>
-            </tr>
+            <TableRow />
+            <TableRow />
           </tbody>
           <tfoot />
         </table>
