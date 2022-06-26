69db7b8fcfd95bcdebb19f274eea72f683d36817
Akram Youssoufi <akram.lyoussoufi@gmail.com>
2022-06-26 12:10:00 +0100

    push

--- src/dashboard/component/Settings/Settings.js
+++ src/dashboard/component/Settings/Settings.js
@@ -1,21 +1,38 @@
 import "./Settings.css";
-import plus from "./Flat_plus_icon.svg.png";
+import plus from "./add.png";
+import Sensors from "./component/Sensors/Sensors";
+import AddSensorForm from "./component/AddBox/AddSensorForm";
+import { useState } from "react";
 
 function Settings() {
+  const [showForm, setShowForm] = useState({ display: "none" });
   return (
     <div className="settings-content">
       <div className="settings-content-container">
+        <div className="addbox" style={showForm}>
+          {" "}
+          <AddSensorForm setShowForm={setShowForm} />
+        </div>
         <div className="settings-side-bar">
           <div className="side-bar-header">
-            <div>Settings</div>
-            <button className="button-add-sensor">
+            <div>Sensors</div>
+            <button
+              className="button-add-sensor"
+              onClick={() => {
+                setShowForm({ display: "block" });
+              }}
+            >
               <img src={plus}></img>
             </button>
           </div>
-          <div className="side-bar-sensors"></div>
+          <div className="side-bar-sensors">
+            <ul>
+              <Sensors />
+            </ul>
+          </div>
         </div>
         <div className="settings-options">
-          <div className="options-header">Sensors</div>
+          <div className="options-header">Settings</div>
           <div className="options-sensors"></div>
         </div>
       </div>
---
+++ src/dashboard/component/Settings/component/Sensors/Sensors.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./Sensors.css";
+export default function Sensors() {
+  return <li className="sensorlist">Sensor 1</li>;
+}
